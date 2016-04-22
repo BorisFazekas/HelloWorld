d5f93da198f5e77e90c62cc3ca6a5b5f378712d3
Petar Vudragovic <petar@w6e-macbookpro.local>
2016-04-22 19:58:27 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Petar Vudragovic on 4/22/16.
+//  Copyright © 2016 Cross Over G5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
